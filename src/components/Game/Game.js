9d730bd2f72f35be3cb1a5ce4f4a173e05d135b1
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-06-16 15:57:10 +0200

    refactor: move GameResults into its own directory

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "./GuessInput";
-import GuessResults from "./GuessResults";
+import GuessResults from "../GuessResults";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
