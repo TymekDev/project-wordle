d2aa5dfa0192e892780ec978d9048975527da01e
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-06-16 16:34:32 +0200

    feat: implement cell coloring

--- src/components/GuessResults/Guess.js
+++ src/components/GuessResults/Guess.js
@@ -5,8 +5,10 @@ function Guess({ checkedGuess }) {
   return (
     <p className="guess">
       {range(NUM_OF_LETTERS).map((index) => {
+        const classes = ["cell", checkedGuess[index]?.status];
+
         return (
-          <span key={index} className="cell">
+          <span key={index} className={classes.join(" ")}>
             {checkedGuess[index]?.letter}
           </span>
         );
